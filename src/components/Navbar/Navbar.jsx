002e1a3003a8a6ef0ddc18173de5033348e012b4
shinnymen <65742813+shinnymen@users.noreply.github.com>
2025-01-02 20:51:10 +0100

    commit nav+carrousel

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -88,7 +88,9 @@ const Navbar = () => {
                     duration-300 rounded-full border 
                     bordergrey-gray-300 px-2 py-1 
                     focus:outline-none focus:border-1 
-                    focus:border-primary'
+                    focus:border-primary
+                    dark:border-gray-500
+                    dark:bg-gray-800'
                     />
                     <IoMdSearch 
                         className='text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3'
