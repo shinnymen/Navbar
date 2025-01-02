acbf86f2f35f50f7e3c2a98f30a1ab768e2d69d4
shinnymen <65742813+shinnymen@users.noreply.github.com>
2025-01-02 19:00:49 +0100

    Navbar ecommerce

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,167 @@
+import React from 'react'
+import Logo from '../../assets/logo.png'
+import { IoMdSearch } from "react-icons/io";
+import { FaCaretDown, FaCartShopping } from 'react-icons/fa6';
+import DarkMode from './DarkMode';
+
+
+const Menu = [
+    {
+        id: 1,
+        name: 'Home',
+        link: '#'
+    },
+    {
+        id: 2,
+        name: 'Products',
+        link: '#'
+    },
+    {
+        id: 3,
+        name: 'About',
+        link: '#'
+    },
+    {
+        id: 4,
+        name: 'Services',
+        link: '#'
+    },
+    {
+        id: 5,
+        name: 'Electronics',
+        link: '#'
+    },
+    {
+        id: 6,
+        name: 'Contact',
+        link: '#'
+    }
+]
+
+const Dropsownlinks = [
+    {
+        id: 1,
+        name: 'Trending Products',
+        link: '#'
+    },
+    {
+        id: 2,
+        name: 'Best Selling',
+        link: '#'
+    },
+    {
+        id: 3,
+        name: 'Top Rated',
+        link: '#'
+    },
+    {
+        id: 4,
+        name: 'Beauty & Health',
+        link: '#'
+    }
+]
+
+
+const Navbar = () => {
+  return (
+  <div className='shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
+  {/* upper navbar */}
+    <div className='bg-primary/40 py-2'>
+        <div className='container flex justify-between items-center'>
+            <div>
+                <a href='#' className='font-bold text-2xl sm:text-3xl flex gap-2'>
+                    <img src={Logo} alt='Logo' 
+                    className='w-10'/>
+                    Ecommerce
+                </a>
+            </div>
+
+                 {/* search bar */}
+            <div className='flex justify-between items-center gap-4'>
+           
+                <div className='relative group hidden sm:block'>
+                    <input 
+                    type='text' 
+                    placeholder='Search' 
+                    className='w-[200px] sm:w-[200px] 
+                    group-hover:w-[300px] transition-all 
+                    duration-300 rounded-full border 
+                    bordergrey-gray-300 px-2 py-1 
+                    focus:outline-none focus:border-1 
+                    focus:border-primary'
+                    />
+                    <IoMdSearch 
+                        className='text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3'
+                    />
+                </div>
+            </div>
+
+            {/* order button */}
+            <button
+            onClick={() => alert('Order not available yet')}
+            className='bg-gradient-to-r from-primary to-secondary transition-all duration-200
+            text-white px-4 py-1 rounded-full flex items-center gap-3 group shadow-md'
+            >
+                <span
+                className='group-hover:block hidden transition-all duration-200'
+                >Order</span>
+                <FaCartShopping
+                className='text-xl text-white drop-shadow-sm cursor-pointer'
+                 />
+            </button>
+
+            {/* Dark button */}
+            <div>
+                <DarkMode  />
+            </div>
+        </div>
+    </div>
+    {/* lower navbar */}
+    <div className='flex justify-center'>
+        <ul className='sm:flex hidden items-center gap-4'>
+            {
+                Menu.map((data) => (
+                    <li key={data.id}>
+                        <a href={data.link} 
+                        className='inline-block px-4 
+                        hover:text-primary duration-200'
+                        >
+                        {data.name}
+                        </a>
+                    </li>
+                ))}
+                {/* Simple Dropdown and Links */}
+                <li className='group relative cursor-pointer'>
+                    <a href='#' 
+                    className='flex items-center 
+                    gap-[2px] py-2'>
+                        Trending Products
+                        <span>
+                            <FaCaretDown 
+                            className='transition-all 
+                            duration-200
+                            group-hover:rotate-180'
+                            />
+                        </span>
+                    </a>
+                    <div className='absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white p-2 text-black shadow-md'>
+                        <ul>
+                            {Dropsownlinks.map((data) => (
+                                <li key={data.id}>
+                                    <a href={data.link}
+                                    className='inline-block w-full rounded-md p-2 hover:bg-primary/20 '
+                                    >
+                                        {data.name}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </li>
+        </ul>
+    </div>
+  </div>
+  )
+}
+
+export default Navbar
